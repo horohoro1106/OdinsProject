2e19209f9195dcf4122876d122aeb6c097b28684
horohoro1106 <horohoro1106@gmail.com>
2018-07-28 01:00:52 +0300

    added tags

--- Web 101/blogger/config/routes.rb
+++ Web 101/blogger/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
+  get '/tags' => 'tags#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
